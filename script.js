b41aa141548d3570f7f2df583420c943359b9f3b
Raffaele Iemma <62607232+raffaeleiemma@users.noreply.github.com>
2026-01-20 16:03:35 +0100

    Random colours and progressive opacity implemented

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let isDrawing = false;
+let isOpacity = false;
+let isRandomColour = false;
+
 const gridMaker = function (size) {
   const grid = document.createElement("div");
   grid.classList.add("grid");
@@ -15,31 +19,91 @@ const gridMaker = function (size) {
   }
 };
 
-const changeColor = function () {
+const draw = function () {
   const cells = document.querySelector(".gridContainer");
   cells.addEventListener("mouseover", function (e) {
-    if (e.target.classList.contains("cell")) {
-      e.target.style.backgroundColor = "black";
+    if (e.target.classList.contains("cell") && isDrawing) {
+      const randomInt = (min, max) =>
+        Math.floor(Math.random() * (max - min + 1) + min);
+      let opacity = 1;
+      let colour = "0,0,0";
+      let currentBG = window.getComputedStyle(e.target).backgroundColor;
+      if (/rgb\(0,\s*0,\s*0\)/.test(currentBG)) return;
+      if (isRandomColour) {
+        colour = `${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)}`;
+      }
+      if (isOpacity) {
+        let currentOpacity = Number(
+          currentBG
+            .slice(currentBG.indexOf("(") + 1, currentBG.indexOf(")"))
+            .split(", ")[3],
+        );
+        if (Number.isNaN(currentOpacity)) {
+          currentOpacity = 1;
+          return;
+        } else currentOpacity += 0.1;
+        e.target.style.backgroundColor = `rgba(${colour},${currentOpacity})`;
+      } else e.target.style.backgroundColor = `rgba(${colour},${opacity})`;
     }
   });
-  // cells.forEach((cell) => {
-  //   cell.addEventListener("mouseover", function () {
-  //     cell.style.backgroundColor = "black";
-  //   });
-  // });
 };
-changeColor();
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.getElementById("myRange");
-  const output = document.getElementById("slider-text");
-  slider.value = 16;
-  output.textContent = "16x16";
-  slider.addEventListener("input", function (e) {
+
+const beginGame = function () {
+  const chooseSize = document.getElementById("chooseSize");
+  chooseSize.addEventListener("click", function (e) {
+    e.preventDefault();
+    let size = Number(prompt("Size: choose a number between 8 and 100"));
+    while (size < 8 || size > 100 || isNaN(size)) {
+      alert("Size should be between 8 and 100");
+      size = Number(prompt("Choose a valid size: "));
+    }
     const grid = document.querySelector(".grid");
     if (grid) grid.remove();
-    const size = e.target.value;
-    output.textContent = `${size}x${size}`;
     gridMaker(size);
+
+    draw();
+    setupDrawingButton();
+    setupOpacityButton();
+    setupRandomColourButton();
   });
-  gridMaker(16);
+};
+const setupDrawingButton = function () {
+  const button = document.getElementById("drawing");
+  button.style.display = "block";
+  button.addEventListener("click", function () {
+    isDrawing = !isDrawing;
+    if (isDrawing) {
+      button.textContent = "Stop drawing!!!";
+    } else {
+      button.textContent = "Start drawing!!!";
+    }
+  });
+};
+const setupOpacityButton = function () {
+  const button = document.getElementById("opacity");
+  button.style.display = "block";
+  button.addEventListener("click", function () {
+    isOpacity = !isOpacity;
+    if (isOpacity) {
+      button.textContent = "Stop progressive opacity!!!";
+    } else {
+      button.textContent = "Start progressive opacity!!!";
+    }
+  });
+};
+const setupRandomColourButton = function () {
+  const button = document.getElementById("random");
+  button.style.display = "block";
+  button.addEventListener("click", function () {
+    isRandomColour = !isRandomColour;
+    if (isRandomColour) {
+      button.textContent = "Stop random colour!!!";
+    } else {
+      button.textContent = "Start random colour!!!";
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  beginGame();
 });
